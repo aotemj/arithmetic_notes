7e82f3aee8332fe30d449a3fd555b6732ca3ac1e
losidk <991807272@qq.com>
2021-02-22 10:26:11 +0800

    feature: binary_search

---
+++ binary_search.js
@@ -0,0 +1,24 @@
+/**
+ * 使用滑动窗口进行二分查找
+ * 思路： 如果
+ */
+
+
+
+function binarySearch(nums, target) {
+    let left = 0, right = nums.length - 1, middle = -1
+    while (left <= right) {
+        middle = parseInt((left + right) / 2)
+        const currentNum = nums[middle]
+        if (currentNum > target) {
+            right = middle - 1
+        } else if (currentNum < target) {
+            left = middle + 1
+        } else {
+            return middle
+        }
+    }
+    return middle
+}
+
+console.log(binarySearch([1, 3, 4, 5, 6, 7, 9, 11, 22, 35], 3));
