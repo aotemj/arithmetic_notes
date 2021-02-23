42be2784d61d2fbed28d9572ba282577a0bca834
losidk <991807272@qq.com>
2021-02-23 10:41:29 +0800

    feature: offer_53 method1

--- offer_53_test.js
+++ offer_53_test.js
@@ -22,16 +22,19 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    const l = nums.length
-    let target = -1
-    for (let i = 0; i < l; i++) {
-        if (nums[i] !== i) {
-            target = i
-            break
+    let left = 0, right = nums.length - 1, mid = 0
+    while (left <= right) {
+        mid = parseInt((left + right) / 2)
+        if (nums[mid] === mid) {
+            left = mid + 1
+        } else {
+            right = mid - 1
         }
     }
-    return target
+    return left
 };
 
 console.log(missingNumber([0, 1, 3]));
 console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 7, 9]));
+console.log(missingNumber([0]));
+console.log(missingNumber([1]));
