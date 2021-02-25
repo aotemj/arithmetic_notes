fa6ae4ba65646905a7018597309e3462847f0fb1
losidk <991807272@qq.com>
2021-02-26 07:46:34 +0800

    feature: 350 method2

--- binary_search/350_test.js
+++ binary_search/350_test.js
@@ -40,31 +40,67 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// var intersect = function (nums1, nums2) {
+//   let res = [], map = {}
+//   let l1 = nums1.length
+//   for (let i = 0; i < l1; i++) {
+//     const item = nums1[i]
+//     if (!map[item]) {
+//       map[item] = 1
+//     } else {
+//       map[item] = map[item] + 1
+//     }
+//   }
+//
+//   // let nums2Trans = [...new Set(nums2)]
+//   let l2 = nums2.length
+//
+//   for (let i = 0; i < l2; i++) {
+//     const item = nums2[i]
+//     if (map[item]) {
+//       map[item] = map[item] - 1
+//       res.push(item)
+//     }
+//   }
+//   return res
+// };
+
+/**
+
+ 方法二：排序 + 双指针
+ 如果两个数组是有序的，则可以使用双指针的方法得到两个数组的交集。
+
+ 首先对两个数组进行排序，然后使用两个指针遍历两个数组。
+
+ 初始时，两个指针分别指向两个数组的头部。每次比较两个指针指向的两个数组中的数字，如果两个数字不相等，则将指向较小数字的指针右移一位，如果两个数字相等，将该数字添加到答案，并将两个指针都右移一位。当至少有一个指针超出数组范围时，遍历结束
+ */
+
 var intersect = function (nums1, nums2) {
-  let res = [], map = {}
-  let l1 = nums1.length
-  for (let i = 0; i < l1; i++) {
-    const item = nums1[i]
-    if (!map[item]) {
-      map[item] = 1
+  let sortedNums1 = nums1.sort((a, b) => a - b)
+  let sortedNums2 = nums2.sort((a, b) => a - b)
+
+  let l1 = sortedNums1.length, l2 = sortedNums2.length
+
+  let p1 = 0, p2 = 0, res = []
+
+  while (p1 < l1 && p2 < l2) {
+    let item1 = sortedNums1[p1]
+    let item2 = sortedNums2[p2]
+    if (item1 === item2) {
+      res.push(item1)
+      p1 += 1
+      p2 += 1
     } else {
-      map[item] = map[item] + 1
+      if (item1 < item2) {
+        p1 += 1
+      } else {
+        p2 += 1
+      }
     }
   }
 
-  // let nums2Trans = [...new Set(nums2)]
-  let l2 = nums2.length
-
-  for (let i = 0; i < l2; i++) {
-    const item = nums2[i]
-    if (map[item]) {
-      map[item] = map[item] - 1
-      res.push(item)
-    }
-  }
   return res
-};
-
+}
 console.log(intersect([1, 2, 2, 1], [2, 2]));
 nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4]
 console.log(intersect(nums1, nums2));
