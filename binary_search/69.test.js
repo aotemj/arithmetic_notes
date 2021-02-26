5af22d5c55d3f71826c8ffaa0304a04cc73fc5bf
losidk <991807272@qq.com>
2021-02-26 15:37:51 +0800

    feature: 69_test method2

--- binary_search/69.test.js
+++ binary_search/69.test.js
@@ -25,13 +25,34 @@
  * @param {number} x
  * @return {number}
  */
+// var mySqrt = function (x) {
+//     if (x === 0) return x
+//     for (let i = 1; i <= x; i++) {
+//         if (i * i === x || ((i - 1) * (i - 1) < x && x < (i + 1) * (i + 1))) {
+//             return i
+//         }
+//     }
+// };
+
+
+/**
+ * method2 binary_search
+ * @param x
+ * @returns {number|*}
+ */
 var mySqrt = function (x) {
-    if (x === 0) return x
-    for (let i = 1; i <= x; i++) {
-        if (i * i === x || ((i - 1) * (i - 1) < x && x < (i + 1) * (i + 1))) {
-            return i
+    let left = 0, right = x
+    while (left <= right) {
+        let mid = parseInt((left + right) / 2)
+        if (mid * mid > x) {
+            right = mid - 1
+        } else if (mid * mid < x) {
+            left = mid + 1
+        } else {
+            return mid
         }
     }
+    return right
 };
 
 console.log(mySqrt(0));
@@ -40,4 +61,5 @@ console.log(mySqrt(2));
 console.log(mySqrt(3));
 console.log(mySqrt(4));
 console.log(mySqrt(8));
+console.log(mySqrt(9));
 console.log(mySqrt(16));
