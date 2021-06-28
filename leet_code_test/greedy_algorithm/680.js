f0a38b6487be4ec94f52760947acd93e8408eaf9
losidk <991807272@qq.com>
2021-06-28 15:32:26 +0800

    680. 验证回文字符串 Ⅱ

---
+++ leet_code_test/greedy_algorithm/680.js
@@ -0,0 +1,56 @@
+/**
+ 680. 验证回文字符串 Ⅱ
+ 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+
+ 示例 1:
+
+ 输入: "aba"
+ 输出: True
+ 示例 2:
+
+ 输入: "abca"
+ 输出: True
+ 解释: 你可以删除c字符。
+ 注意:
+
+ 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+    const length = s.length;
+    let left = 0, right = length - 1;
+    while (left < right) {
+        if (s[left] === s[right]) {
+            left++;
+            right--;
+        } else {
+            const leftSub = s.substring(0, left) + s.substring(left + 1)
+            const rightSub = s.substring(0, right) + s.substring(right + 1)
+            return isPalindrome(leftSub) || isPalindrome(rightSub)
+        }
+    }
+    return true
+
+    function isPalindrome(s) {
+        const l = s.length
+        for (let i = 0; i < l; i++) {
+            if (s[i] !== s[l - i - 1]) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
+
+const s = "aba"
+// const s = "abca"
+// const s = "tebbem"
+// const s = "ababa"
+// const s = "bddb"
+// const s = "deeee"
+console.log(validPalindrome(s));
