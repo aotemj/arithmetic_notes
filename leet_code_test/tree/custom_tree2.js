ce53cdb8c7cbd7e7e900779296c6f90afe3b41d7
ceui <991807272@qq.com>
2021-12-04 07:02:40 +0800

    custom_tree2

---
+++ leet_code_test/tree/custom_tree2.js
@@ -0,0 +1,29 @@
+/**
+ * 二叉树光照节点
+ * 从右侧有一束光照过来，请输出光能照到的所有节点
+ *
+ *  例如： 给定二叉树： [3,9,20,null,null,15,7]
+ *
+ *            3
+ *           / \
+ *          9  20
+ *            /  \
+ *           15   7
+ *   输出：
+ *   [
+ *      [3],
+ *      [9,20],
+ *      [15,7]
+ *   ]
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+var levelOrder = function (root) {
+  return res
+}
